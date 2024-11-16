7a01ea6301ab5e6157d0bcff9ae88868b10843c6
efraimmf <emfs6@aluno.ifal.edu.br>
2024-11-15 22:02:44 -0300

    feat: update login page layout
    - Added new linear layouts for login page.
    - Added custom font to buttons and text views.
    - Implemented borders for EditText fields.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.project_hurb"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.project_hurb"
